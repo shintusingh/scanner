95e220f8d58e31a0d930d250f574be411101f784
root <shintu.singh@axtrics.com>
2020-09-15 16:11:51 +0530

    Update code2

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import fingertprint from "./components/fingertprint";
 
 export default function App() {
   return (
     <SafeAreaView styles={styles.container}>
-      <Text>Test</Text>
+      <Text>Test sdffffffffff</Text>
+      <fingertprint></fingertprint>
     </SafeAreaView>
   );
 }
