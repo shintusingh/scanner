51ed1dd09c20d97f998007c4f7097b29ddf73216
root <shintu.singh@axtrics.com>
2020-09-16 11:11:21 +0530

    Update code5

--- components/fingertprint.js
+++ components/fingertprint.js
@@ -19,6 +19,7 @@ export default class fingertprint extends Component {
   };
 
   componentDidMount() {
+    alert("oookkk");
     this.checkDeviceForHardware();
     this.checkForFingerprints();
   }
