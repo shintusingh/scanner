43e9c8bbd91b0a6897df7472af467a5c3a88d6fa
root <shintu.singh@axtrics.com>
2020-09-16 13:25:48 +0530

    Update code6

--- components/fingertprint.js
+++ components/fingertprint.js
@@ -10,80 +10,27 @@ import {
 } from "react-native";
 import { LocalAuthentication } from "expo";
 import Constants from "expo-constants";
-
 export default class fingertprint extends Component {
-  state = {
-    compatible: false,
-    fingerprints: false,
-    result: "",
-  };
-
-  componentDidMount() {
-    this.checkDeviceForHardware();
-    this.checkForFingerprints();
-  }
-
-  checkDeviceForHardware = async () => {
-    let compatible = await LocalAuthentication.hasHardwareAsync();
-    this.setState({ compatible });
-  };
-
-  checkForFingerprints = async () => {
-    let fingerprints = await LocalAuthentication.isEnrolledAsync();
-    this.setState({ fingerprints });
-  };
-
-  scanFingerprint = async () => {
-    let result = await LocalAuthentication.authenticateAsync(
-      "Scan your finger."
-    );
-    console.log("Scan Result:", result);
-    this.setState({
-      result: JSON.stringify(result),
-    });
-  };
-
-  showAndroidAlert = () => {
-    Alert.alert(
-      "Fingerprint Scan",
-      "Place your finger over the touch sensor and press scan.",
-      [
-        {
-          text: "Scan",
-          onPress: () => {
-            this.scanFingerprint();
-          },
-        },
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel"),
-          style: "cancel",
-        },
-      ]
-    );
-  };
-
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          Compatible Device? {this.state.compatible === true ? "True" : "False"}
+        <Text>
+          On the other hand, we denounce with righteous indignation and dislike
+          men who are so beguiled and demoralized by the charms of pleasure of
+          the moment, so blinded by desire, that they cannot foresee the pain
+          and trouble that are bound to ensue; and equal blame belongs to those
+          who fail in their duty through weakness of will, which is the same as
+          saying through shrinking from toil and pain. These cases are perfectly
+          simple and easy to distinguish. In a free hour, when our power of
+          choice is untrammelled and when nothing prevents our being able to do
+          what we like best, every pleasure is to be welcomed and every pain
+          avoided. But in certain circumstances and owing to the claims of duty
+          or the obligations of business it will frequently occur that pleasures
+          have to be repudiated and annoyances accepted. The wise man therefore
+          always holds in these matters to this principle of selection: he
+          rejects pleasures to secure other greater pleasures, or else he
+          endures pains to avoid worse pains
         </Text>
-        <Text style={styles.text}>
-          Fingerprings Saved?{" "}
-          {this.state.fingerprints === true ? "True" : "False"}
-        </Text>
-        <TouchableOpacity
-          onPress={
-            Platform.OS === "android"
-              ? this.showAndroidAlert
-              : this.scanFingerprint
-          }
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>SCAN</Text>
-        </TouchableOpacity>
-        <Text>{this.state.result}</Text>
       </View>
     );
   }
